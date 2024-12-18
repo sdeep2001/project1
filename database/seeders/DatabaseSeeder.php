899468158db445bdec70fcd8d87528dd114eb721
Md.Hasib Ur Rahman <mohammod.hasib@g.bracu.ac.bd>
2024-12-18 22:42:23 +0600

    chore: add database seeder for users table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,31 @@ class DatabaseSeeder extends Seeder
 
         $users = User::all()->shuffle();
 
-        
+        for ($i = 0; $i < 20; $i++) {
+            \App\Models\Employer::factory()->create([
+                'user_id' => $users->pop()->id
+            ]);
+        }
+
+        $employers = \App\Models\Employer::all();
+
+        for ($i = 0; $i < 100; $i++) {
+            \App\Models\Job::factory()->create([
+                'employer_id' => $employers->random()->id
+            ]);
+        }
+
+        foreach ($users as $user) {
+            $jobs = \App\Models\Job::inRandomOrder()
+                ->take(rand(0, 4))
+                ->get();
+
+            foreach ($jobs as $job) {
+                \App\Models\JobApplication::factory()->create([
+                    'job_id' => $job->id,
+                    'user_id' => $user->id
+                ]);
             }
         }
-    
+    }
+}
